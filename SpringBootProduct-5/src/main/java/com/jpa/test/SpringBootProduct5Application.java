9c038d76e606cc06a752da45de5a5085a99650e3
Rushikesh <Rushikesh@DESKTOP-5RFJV5I>
2024-04-26 23:45:19 +0530

    CRUD Assignment

---
+++ SpringBootProduct-5/src/main/java/com/jpa/test/SpringBootProduct5Application.java
@@ -0,0 +1,15 @@
+package com.jpa.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+
+@SpringBootApplication
+
+public class SpringBootProduct5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootProduct5Application.class, args);
+	}
+
+}
